a1da5f67f53df2c7821738f78dcb0a7a7b579c70
Abe Howder <ahowder@cedarville.edu>
2024-09-06 14:03:17 -0400

    Initial Commit

---
+++ Lab03/Lab03App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab03App.swift
+//  Lab03
+//
+//  Created by Abe Howder on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
