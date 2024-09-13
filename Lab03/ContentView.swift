90cace70189f5b393f55b5674516efb43541afb1
Abe Howder <ahowder@cedarville.edu>
2024-09-12 20:43:08 -0400

    Initial Commit

--- Lab03/ContentView.swift
+++ Lab03/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let restaurants = ["McDonald's", "Arby's", "Wendy's", "Pizza Hut", "Culver's"]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(restaurants, id: \.self) { restaurant in
+                RestaurantView(name: restaurant, isFavorite: false)
+            }
         }
         .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
 #Preview {
     ContentView()
 }
+import SwiftUI
+
+struct RestaurantView: View {
+    var name: String
+    @State var isFavorite: Bool
+
+    var body: some View {
+        HStack {
+            Text(name)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            Spacer()
+            Text(isFavorite ? "👍" : "👎")
+                .font(.largeTitle)
+        }
+        .padding(50)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .onTapGesture {
+            isFavorite.toggle()
+        }
+    }
+}
+
